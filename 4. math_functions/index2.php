929fc246db2e6416764779faf05fe096412cf903
michelmlg <147891522+michelmlg@users.noreply.github.com>
2024-07-07 15:30:42 -0300

    math-functions

---
+++ 4. math_functions/index2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index2.php" method="post">
+        <label>radius:</label>
+        <input type="text" name="radius">
+        <input type="submit" value="calculate">
+    </form>
+</body>
+</html>
+
+<?php
+    $radius = $_POST["radius"];
+    $circumference = null;
+    $area = null;
+
+    $circumference = 2 * pi() * $radius;
+    $circumference = round($circumference, 2);
+
+    $area = pi() * pow($radius, 2);
+    $area = round($area, 2);
+
+    $volume = 4/3 * pi() * pow($radius, 3);
+    $volume = round($volume, 2);
+
+    echo "Circumference = {$circumference}cm <br>";
+    echo "Area = {$area}cm² <br>";
+    echo "Volume = {$volume}cm³ <br>"
+?>
